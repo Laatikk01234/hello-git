a733e3fb647d8d0042678299cb44561ec9735cff
Jyry <Jyry.uitto@gmail.com>
2021-09-09 15:51:38 +0300

    copy pasted the wrong version

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,4 +1,3 @@
-
 def debug_print(debug_msg=None, **kwargs):
 
     if debug_msg:
@@ -45,15 +44,6 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
 
-    input_list = input_str.split(",")
-    debug_print(input_list=input_list)
-
-    value_list = []
-    for x in input_list:
-        try:
-            value_list.append(int(x))
-        except ValueError as err:
-            print("Invalid input.")
             quit(1)
 
     debug_print(value_list=value_list)
